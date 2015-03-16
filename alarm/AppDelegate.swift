f370825e10c8327c86abfb000aec481b28aa0ba9
Mike Lewis <mlewis.mail@gmail.com>
2015-03-15 17:18:47 -0700

    use the mic to record and check sound every 5s

--- alarm/AppDelegate.swift
+++ alarm/AppDelegate.swift
@@ -35,9 +35,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
       window.makeKeyAndVisible()
 
-      // Request location access
+      // Request location and recording access
       // We should move this into the onboarding
       LocationHelper.requestLocationAccess()
+      SoundMonitorHelper.requestPermissionIfNeeded()
+
+      SoundMonitorHelper.startRecording()
     }
     
     return true
@@ -50,24 +53,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func applicationWillResignActive(application: UIApplication) {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    NSLog("applicationWillResignActive")
   }
 
   func applicationDidEnterBackground(application: UIApplication) {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    NSLog("applicationDidEnterBackground")
   }
 
   func applicationWillEnterForeground(application: UIApplication) {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    NSLog("applicationWillEnterForeground")
   }
 
   func applicationDidBecomeActive(application: UIApplication) {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    NSLog("applicationDidBecomeActive")
   }
 
   func applicationWillTerminate(application: UIApplication) {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     // Saves changes in the application's managed object context before the application terminates.
+    NSLog("applicationWillTerminate")
     MagicalRecord.cleanUp()
     self.saveContext()
   }
---
+++ alarm/SoundMonitorHelper.swift
@@ -0,0 +1,105 @@
+//
+//  SoundMonitorHelper.swift
+//  alarm
+//
+//  Created by Michael Lewis on 3/15/15.
+//  Copyright (c) 2015 Kevin Farst. All rights reserved.
+//
+
+import AVFoundation
+import Foundation
+
+
+// Stash a singleton global instance
+private let _soundMonitorHelper = SoundMonitorHelper()
+
+class SoundMonitorHelper: NSObject, AVAudioRecorderDelegate {
+  let recordSettings = [
+    AVFormatIDKey: kAudioFormatAppleLossless,
+    AVEncoderAudioQualityKey : AVAudioQuality.Min.rawValue,
+    AVEncoderBitRateKey : 64000,
+    AVNumberOfChannelsKey: 1,
+    AVSampleRateKey : 44100.0,
+  ]
+  // We don't need to save the recorded sound. We just
+  // want to monitor it's levels as it's being recorded.
+  let soundFileURL = NSURL(fileURLWithPath: "/dev/null")
+  let session: AVAudioSession = AVAudioSession.sharedInstance()
+  let recorder: AVAudioRecorder
+
+  // A periodic time checks levels
+  var meterTimer: NSTimer?
+
+  override init() {
+    var sessionError: NSError?
+    session = AVAudioSession.sharedInstance()
+    session.setCategory(AVAudioSessionCategoryRecord, error: &sessionError)
+    if let e = sessionError {
+      println(e.localizedDescription)
+    }
+
+    var recorderError: NSError?
+    recorder = AVAudioRecorder(
+      URL: soundFileURL!,
+      settings: recordSettings,
+      error: &recorderError
+    )
+
+    super.init()
+
+    if let e = recorderError {
+      println(e.localizedDescription)
+    } else {
+      recorder.delegate = self
+      recorder.meteringEnabled = true
+    }
+  }
+
+  // Create and hold onto a singleton instance of this class
+  class var singleton: SoundMonitorHelper {
+    return _soundMonitorHelper
+  }
+
+  class func requestPermissionIfNeeded() {
+    singleton.session.requestRecordPermission({
+      (granted: Bool)-> Void in
+
+    })
+  }
+
+  class func startRecording() {
+    singleton.recorder.record()
+
+    singleton.meterTimer = NSTimer.scheduledTimerWithTimeInterval(
+      5.0, // seconds
+      target: singleton,
+      selector: "updateAudioMeter:",
+      userInfo: nil,
+      repeats: true
+    )
+  }
+
+  func audioRecorderDidFinishRecording(recorder: AVAudioRecorder!, successfully flag: Bool) {
+    NSLog("finished recording \(flag)")
+  }
+
+  func audioRecorderEncodeErrorDidOccur(recorder: AVAudioRecorder!, error: NSError!) {
+    NSLog("\(error.localizedDescription)")
+  }
+
+  func updateAudioMeter(timer:NSTimer) {
+    if recorder.recording {
+      recorder.updateMeters()
+      var apc0  = recorder.averagePowerForChannel(0)
+      var peak0 = recorder.peakPowerForChannel(0)
+
+      let dFormat = "%02d"
+      NSLog("seconds: \(recorder.currentTime), avg: \(apc0), peak: \(peak0)")
+    }
+  }
+
+  // Returns true if we need to ask for microphone permission
+  private func needsPermission() -> Bool {
+    return AVAudioSessionRecordPermission.Undetermined == session.recordPermission()
+  }
+}
